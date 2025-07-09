d47c79c7df3a551756b2386cb269ce3a564a6f3d
yidongseok <yidongseok@gmail.com>
2025-07-09 10:46:40 +0900

    초기 커밋.

---
+++ ExcelSheetAutoPrinter/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ExcelSheetAutoPrinter
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnLoadExcel = new System.Windows.Forms.Button();
+			this.txtSrcFilePath = new System.Windows.Forms.TextBox();
+			this.btnFileSelect = new System.Windows.Forms.Button();
+			this.lblSrcFile = new System.Windows.Forms.Label();
+			this.txtDestFilePath = new System.Windows.Forms.TextBox();
+			this.lblDestFile = new System.Windows.Forms.Label();
+			this.gvExcel = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.gvExcel)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnLoadExcel
+			// 
+			this.btnLoadExcel.Location = new System.Drawing.Point(16, 93);
+			this.btnLoadExcel.Name = "btnLoadExcel";
+			this.btnLoadExcel.Size = new System.Drawing.Size(163, 23);
+			this.btnLoadExcel.TabIndex = 0;
+			this.btnLoadExcel.Text = "엑셀불러오기, PDF저장";
+			this.btnLoadExcel.UseVisualStyleBackColor = true;
+			this.btnLoadExcel.Click += new System.EventHandler(this.btnLoadExcel_Click);
+			// 
+			// txtSrcFilePath
+			// 
+			this.txtSrcFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtSrcFilePath.Location = new System.Drawing.Point(114, 39);
+			this.txtSrcFilePath.Name = "txtSrcFilePath";
+			this.txtSrcFilePath.Size = new System.Drawing.Size(942, 21);
+			this.txtSrcFilePath.TabIndex = 1;
+			// 
+			// btnFileSelect
+			// 
+			this.btnFileSelect.Location = new System.Drawing.Point(14, 10);
+			this.btnFileSelect.Name = "btnFileSelect";
+			this.btnFileSelect.Size = new System.Drawing.Size(165, 23);
+			this.btnFileSelect.TabIndex = 2;
+			this.btnFileSelect.Text = "파일 선택";
+			this.btnFileSelect.UseVisualStyleBackColor = true;
+			this.btnFileSelect.Click += new System.EventHandler(this.btnFileSelect_Click);
+			// 
+			// lblSrcFile
+			// 
+			this.lblSrcFile.AutoSize = true;
+			this.lblSrcFile.Location = new System.Drawing.Point(11, 42);
+			this.lblSrcFile.Name = "lblSrcFile";
+			this.lblSrcFile.Size = new System.Drawing.Size(97, 12);
+			this.lblSrcFile.TabIndex = 3;
+			this.lblSrcFile.Text = "엑셀 파일 경로 : ";
+			// 
+			// txtDestFilePath
+			// 
+			this.txtDestFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtDestFilePath.Location = new System.Drawing.Point(114, 66);
+			this.txtDestFilePath.Name = "txtDestFilePath";
+			this.txtDestFilePath.Size = new System.Drawing.Size(942, 21);
+			this.txtDestFilePath.TabIndex = 4;
+			// 
+			// lblDestFile
+			// 
+			this.lblDestFile.AutoSize = true;
+			this.lblDestFile.Location = new System.Drawing.Point(11, 69);
+			this.lblDestFile.Name = "lblDestFile";
+			this.lblDestFile.Size = new System.Drawing.Size(96, 12);
+			this.lblDestFile.TabIndex = 5;
+			this.lblDestFile.Text = "PDF 파일 경로 : ";
+			// 
+			// gvExcel
+			// 
+			this.gvExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.gvExcel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.gvExcel.Location = new System.Drawing.Point(16, 122);
+			this.gvExcel.Name = "gvExcel";
+			this.gvExcel.RowTemplate.Height = 23;
+			this.gvExcel.Size = new System.Drawing.Size(1040, 356);
+			this.gvExcel.TabIndex = 6;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1070, 762);
+			this.Controls.Add(this.gvExcel);
+			this.Controls.Add(this.lblDestFile);
+			this.Controls.Add(this.txtDestFilePath);
+			this.Controls.Add(this.lblSrcFile);
+			this.Controls.Add(this.btnFileSelect);
+			this.Controls.Add(this.txtSrcFilePath);
+			this.Controls.Add(this.btnLoadExcel);
+			this.Name = "frmMain";
+			this.Text = "Excel Sheet Auto Printer";
+			((System.ComponentModel.ISupportInitialize)(this.gvExcel)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnLoadExcel;
+		private System.Windows.Forms.TextBox txtSrcFilePath;
+		private System.Windows.Forms.Button btnFileSelect;
+		private System.Windows.Forms.Label lblSrcFile;
+		private System.Windows.Forms.TextBox txtDestFilePath;
+		private System.Windows.Forms.Label lblDestFile;
+		private System.Windows.Forms.DataGridView gvExcel;
+	}
+}
+
---
+++ ExcelSheetAutoPrinter/Form1.cs
@@ -0,0 +1,144 @@
+﻿using ExcelDataReader;
+using log4net;
+using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelSheetAutoPrinter
+{
+	public partial class frmMain : Form
+	{
+        private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		private string pdfFileName = string.Empty; // PDF 파일 이름
+
+		public frmMain()
+		{
+			InitializeComponent();
+
+			InitForm();
+		}
+
+		private void InitForm()
+		{
+			//txtFileName.Text = @"D:\ExcelSheetAutoPrinter\ExcelSheetAutoPrinter\디아블로4_영원_원소술사(제압번개창-푸들멈머)_yidongseok@gmail.com.xlsx";
+		}
+
+		private void btnLoadExcel_Click(object sender, EventArgs e)
+		{
+			LoadExcelFile(txtSrcFilePath.Text);
+		}
+
+		private void LoadExcelFile(string text)
+		{
+			Excel.Application excelApp = null;
+			Excel.Workbook workBook = null;
+			Excel.Worksheet workSheet = null;
+			string path = txtSrcFilePath.Text;
+
+            try
+			{
+				excelApp = new Excel.Application();											// 엑셀 어플리케이션 생성
+                workBook = excelApp.Workbooks.Open(path);									// 워크북 열기
+                workSheet = workBook.Worksheets.get_Item(4) as Excel.Worksheet;				// 엑셀 첫번째 워크시트 가져오기
+
+				Excel.Range range = workSheet.UsedRange;									// 사용중인 셀 범위를 가져오기
+
+				for (int columnNo = 1; columnNo <= range.Columns.Count; columnNo++)         // 가져온 열 만큼 반복
+				{
+					string strColumnName = (string)(range.Cells[1, columnNo] as Excel.Range).Value2; // 첫번째 행의 셀 값 가져오기
+
+					gvExcel.Columns.Add(strColumnName, strColumnName);										// 데이터 그리드뷰에 열 추가
+				}
+
+				for (int rowNo = 1; rowNo <= range.Rows.Count; rowNo++)						// 가져온 행 만큼 반복
+                {
+					DataGridViewRow row = new DataGridViewRow();							// 데이터 그리드뷰 행 생성
+					for (int columnNo = 1; columnNo <= range.Columns.Count; columnNo++)		// 가져온 열 만큼 반복
+                    {
+                        string str = (string)(range.Cells[rowNo, columnNo] as Excel.Range).Value2;  // 셀 데이터 가져옴
+
+                        //logger.Debug(str + " ");
+
+						Excel.Range cell = range.Cells[rowNo, columnNo] as Excel.Range;				// 셀 객체 가져오기
+						DataGridViewTextBoxCell cellControl = new DataGridViewTextBoxCell();		// 데이터 그리드뷰 텍스트 박스 셀 생성
+						cellControl.Value = (cell.Value == null) ? "" : cell.Value.ToString();		// 셀 값 설정
+						row.Cells.Add(cellControl);													// 행에 셀 추가
+					}
+
+					gvExcel.Rows.Add(row); // 데이터 그리드뷰에 행 추가
+
+					logger.Debug("");
+                }
+
+                workSheet.ExportAsFixedFormat(Excel.XlFixedFormatType.xlTypePDF, Path.Combine(Path.GetDirectoryName(path), pdfFileName)); // 워크시트 PDF로 저장
+
+				workBook.Close(true);   // 워크북 닫기
+                excelApp.Quit();        // 엑셀 어플리케이션 종료
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+			}
+			finally
+			{
+				ReleaseObject(workSheet);
+                ReleaseObject(workBook);
+                ReleaseObject(excelApp);
+			}
+		}
+
+		/// <summary>
+        /// 액셀 객체 해제 메소드
+        /// </summary>
+        /// <param name="obj"></param>
+        private void ReleaseObject(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj);  // 액셀 객체 해제
+                    obj = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+            }
+            finally
+            {
+                GC.Collect();                       // 가비지 수집
+            }
+        }
+
+		private void btnFileSelect_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				OpenFileDialog ofd = new OpenFileDialog();
+
+				ofd.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm;*.xlsb|All Files|*.*";	// 엑셀 파일 필터
+				ofd.ShowDialog();														// 파일 선택 대화창 표시
+
+				if (ofd.FileName != string.Empty)										// 파일이 선택되었으면
+				{
+					txtSrcFilePath.Text = ofd.FileName;									// 텍스트 박스에 파일 경로 표시
+
+					pdfFileName = Path.GetFileNameWithoutExtension(ofd.FileName) + ".pdf"; // PDF 파일 이름 설정
+
+					txtDestFilePath.Text = Path.Combine(Path.GetDirectoryName(ofd.FileName), pdfFileName); // PDF 파일 경로 설정
+				}
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+			}
+		}
+	}
+}

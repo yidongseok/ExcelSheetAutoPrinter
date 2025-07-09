f3a96899da0a978ce2d4f1c3dc7d572e49423e04
yidongseok <yidongseok@gmail.com>
2025-07-09 15:52:35 +0900

    Scheduler 라이브러리, 클래스 추가

---
+++ ExcelSheetAutoPrinter/JobInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExcelSheetAutoPrinter
+{
+	internal class JobInfo
+	{
+		/// <summary>
+        /// Job Key
+        /// </summary>
+        public string Key
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Corn 표현식
+        /// </summary>
+        public string CronExpression
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Job 시작 시간
+        /// </summary>
+        public DateTime StartTime
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Job 종료 시간
+        /// </summary>
+        public DateTime EndTime
+        {
+            get;
+            set;
+        }
+	}
+}
---
+++ ExcelSheetAutoPrinter/TestJob.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using Quartz;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ExcelSheetAutoPrinter
+{
+	internal class TestJob : IJob
+	{
+		private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public async Task Execute(IJobExecutionContext context)
+		{
+			try
+			{
+				// Log the job execution
+				logger.Info("TestJob is executing at: " + DateTime.Now);
+				// Simulate some work
+				await Task.Delay(1000); // Simulate a delay of 1 second
+				// Log completion
+				logger.Info("TestJob completed at: " + DateTime.Now);
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+			}
+		}
+	}
+}
